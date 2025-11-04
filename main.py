e8be8ec488c8ce010490f33091161b8c027f6f43
black-RAM <pplanes311@gmail.com>
2025-11-04 21:25:00 +0300

    improve selection and filtering so list of mapbox font names has only font names

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ driver.get("https://github.com/mapbox/mapbox-studio-default-fonts/tree/master")
 time.sleep(2)
 
 # scrape text in links to folders/files
-links = driver.find_elements(By.CLASS_NAME, "Link--primary")
+links = driver.find_elements(By.CSS_SELECTOR, "tbody .Link--primary")
 texts = [link.text for link in links]
 
 def is_folder_name(name: str) -> bool:
     """Return True if the name does not have a file extension (is a folder)."""
-    return not bool(os.path.splitext(name)[1])
+    return name and not bool(os.path.splitext(name)[1]) and not name.startswith(".")
 
 # I'm assuming that all the folder names are fonts.
 mapbox_font_names = list(filter(is_folder_name, texts))
