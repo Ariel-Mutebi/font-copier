74f5372d04fe70e4287d8b0a3a703d9969185968
black-RAM <pplanes311@gmail.com>
2025-11-05 17:04:02 +0300

    feat: scrape fonts from mapka github and compare to see fonts mapbox has that we don't

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 import os
 import time
+from typing import List
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-# Configure Chrome options
 options = Options()
-options.add_argument("--start-maximized")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-# Mapbox fonts page
-driver.get("https://github.com/mapbox/mapbox-studio-default-fonts/tree/master")
+def scrape_folder_names_from_github_page(github_page_url: str) -> List[str]:
+    driver.get(github_page_url)
+    time.sleep(2)
 
-time.sleep(2)
+    links = driver.find_elements(By.CSS_SELECTOR, "tbody .Link--primary")
+    texts = [link.text for link in links]
 
-# scrape text in links to folders/files
-links = driver.find_elements(By.CSS_SELECTOR, "tbody .Link--primary")
-texts = [link.text for link in links]
+    def is_folder_name(name: str) -> bool:
+        return name and not bool(os.path.splitext(name)[1]) and not name.startswith(".")
 
-def is_folder_name(name: str) -> bool:
-    """Return True if the name does not have a file extension (is a folder)."""
-    return name and not bool(os.path.splitext(name)[1]) and not name.startswith(".")
+    return list(filter(is_folder_name, texts))
 
-# I'm assuming that all the folder names are fonts.
-mapbox_font_names = list(filter(is_folder_name, texts))
-print(mapbox_font_names)
+mapbox_font_names = scrape_folder_names_from_github_page("https://github.com/mapbox/mapbox-studio-default-fonts/tree/master")
+mapka_font_names = scrape_folder_names_from_github_page("https://github.com/mapka-dev/fonts/tree/master/packages/fonts/fonts")
+
+def capitalised_snake_case_to_kebab_case(s: str) -> str:
+    return s.replace("_", "-").lower()
+
+mapka_font_names = [capitalised_snake_case_to_kebab_case(name) for name in mapka_font_names]
+fonts_to_add = [font_name for font_name in mapbox_font_names if not font_name in mapka_font_names]
+print(fonts_to_add)
 
 driver.quit()
